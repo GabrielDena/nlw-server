263cc569559a9ba5f14b802f8da93b7b10732dcd
Gabriel Dena <gabrieldenac@hotmail.com>
2023-05-24 20:53:02 -0300

    + Uploads + Serving static files * Class 4

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import cors from '@fastify/cors';
 import jwt from '@fastify/jwt';
+import multipart from '@fastify/multipart';
 import 'dotenv/config';
 import fastify from 'fastify';
+import { resolve } from 'node:path';
 import { authRoutes } from './routes/auth.routes';
 import { memoriesRoutes } from './routes/memories.routes';
+import { uploadRoutes } from './routes/upload.routes';
 
 const app = fastify();
+
+app.register(multipart);
+
+app.register(require('@fastify/static'), {
+    root: resolve(process.cwd(), 'uploads'),
+    prefix: '/uploads',
+});
 
 app.register(cors, {
     origin: true,
@@ -17,6 +27,7 @@ app.register(jwt, {
 
 app.register(memoriesRoutes, { prefix: 'memories' });
 app.register(authRoutes);
+app.register(uploadRoutes, { prefix: 'upload' });
 
 app.listen({
     port: 3333,
